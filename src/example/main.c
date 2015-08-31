7c4ac74f4332c6e489e2a0b9daf7fe4ebdb5dd7c
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-08-30 19:09:22 -0500

    Update main.c

--- src/example/main.c
+++ src/example/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  printf("Operating systems, ITESO, Autumn 2015 :D\n");
+  printf("Operating systems, ITESO, Autumn 2015\n");
   return 0;
 }
