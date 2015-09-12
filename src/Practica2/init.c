21829569a163d3f5696280b557a498078caea8c3
Alberto Arias Azcona <maug.thief@gmail.com>
2015-09-12 18:10:29 -0500

    Changed indentation to 2 spaces for init

--- src/Practica2/init.c
+++ src/Practica2/init.c
@@ -3,25 +3,24 @@
 
 int main(){
 
-	int pid;
-	int i;
-	int status; //may not be needed
+  int pid;
+  int i;
+  int status; //may not be needed
 
-	//creates 6 procesess and replaces them with getty
-	for (i = 0; i < 6; ++i){
-		pid = fork();
-		if(pid == 0)
-			execlp("xterm", "xterm -e", "./dummygetty", NULL);
-		
-	}
+  //creates 6 procesess and replaces them with getty
+  for (i = 0; i < 6; ++i){
+    pid = fork();
+    if(pid == 0)
+      execlp("xterm", "xterm -e", "./dummygetty", NULL);
 
-	while(1){
+    }
+
+  while(1){
 		
-		wait(&status);
-		pid = fork();
-		if(pid == 0)
-			execlp("xterm", "xterm -e", "./dummygetty", NULL);
-	}
+    wait(&status);
+    pid = fork();
+    if(pid == 0)
+      execlp("xterm", "xterm -e", "./dummygetty", NULL);
+    }
 	
-
 }
