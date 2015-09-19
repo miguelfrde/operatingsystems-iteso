703403cc01d87510eca982c9c077069182004102
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-09-19 16:57:35 -0500

    Fix code style

--- src/practica3/main.c
+++ src/practica3/main.c
@@ -5,145 +5,151 @@
 
 #define DIF 16
 
-// NOMBRE DEL ARCHIVO A PROCESAR
-char filename[]="/home/usuario/imagen.bmp";
+// File top process
+char filename[] = "/home/usuario/imagen.bmp";
 
-#pragma pack(2)  // Empaquetado de 2 bytes
+#pragma pack(2)  // 2 bytes packaging
 typedef struct {
   unsigned char magic1;
   unsigned char magic2;
   unsigned int size;
   unsigned short int reserved1, reserved2;
-  unsigned int pixelOffset; // offset a la imagen
-} HEADER;
+  unsigned int pixel_offset; // image offset
+} Header;
 
-#pragma pack() // Empaquetamiento por default
+#pragma pack() // Default packaging
 typedef struct {
-  unsigned int size; // Tamaño de este encabezado INFOHEADER
-  int cols, rows; // Renglones y columnas de la imagen
+  unsigned int size; // Size of this header
+  int cols, rows; // Image columns and rows
   unsigned short int planes;
-  unsigned short int bitsPerPixel; // Bits por pixel
+  unsigned short int bits_per_pixel; // Bits per pixel
   unsigned int compression;
-  unsigned int cmpSize;
-  int xScale, yScale;
-  unsigned int numColors;
-  unsigned int importantColors;
-} INFOHEADER;
+  unsigned int cmp_size;
+  int x_scale, y_scale;
+  unsigned int num_colors;
+  unsigned int important_colors;
+} InfoHeader;
 
 typedef struct {
-      unsigned char red;
-      unsigned char green;
-      unsigned char blue;
-} PIXEL;
+  unsigned char red;
+  unsigned char green;
+  unsigned char blue;
+} Pixel;
 
 typedef struct {
-      HEADER header;
-      INFOHEADER infoheader;
-      PIXEL *pixel;
-} IMAGE;
+  Header header;
+  InfoHeader infoheader;
+  Pixel *pixel;
+} Image;
 
-IMAGE imagenfte,imagendst;
+Image source_image, dest_image;
 
-int loadBMP(char *filename, IMAGE *image) {
-  FILE *fin;
-  int i=0;
-  int totpixs=0;
-  fin = fopen(filename, "rb+");
+int load_bmp(char* filename, Image* image) {
+  int totpixs = 0;
+  FILE* fp = fopen(filename, "rb+");
 
-  // Si el archivo no existe
-  if (fin == NULL)
-    return(-1);
-
-  // Leer encabezado
-  fread(&image->header, sizeof(HEADER), 1, fin);
-
-  // Probar si es un archivo BMP
-  if (!((image->header.magic1 == 'B') && (image->header.magic2 == 'M')))
-    return(-1);
-
-  fread(&image->infoheader, sizeof(INFOHEADER), 1, fin);
-  // Probar si es un archivo BMP 24 bits no compactado
-
-  if (!((image->infoheader.bitsPerPixel == 24) && !image- >infoheader.compression))
-    return(-1);
-
-  image->pixel=(PIXEL *)malloc(sizeof(PIXEL)*image->infoheader.cols*image->infoheader.rows);
-  totpixs=image->infoheader.rows*image->infoheader.cols;
-
-  while(i<totpixs) {
-    fread(image->pixel+i, sizeof(PIXEL),512, fin); i+=512;
+  if (fp == NULL) {
+    return -1;
   }
 
-  fclose(fin);
-}
+  // Read header
+  fread(&image->header, sizeof(Header), 1, fp);
 
-int saveBMP(char *filename, IMAGE *image) {
-  FILE *fout;
-  int i,totpixs;
-  fout=fopen(filename,"wb");
-  if (fout == NULL)
-    return(-1); // Error
-
-  // Escribe encabezado
-  fwrite(&image->header, sizeof(HEADER), 1, fout);
-
-  // Escribe información del encabezado
-  fwrite(&image->infoheader, sizeof(INFOHEADER), 1, fout);
-
-  i=0;
-  totpixs=image->infoheader.rows*image->infoheader.cols;
-  while(i<totpixs) {
-    fwrite(image->pixel+i,sizeof(PIXEL),512,fout); i+=512;
+  // Check if its a BMP file
+  if (!(image->header.magic1 == 'B' && image->header.magic2 == 'M')) {
+    return -1;
   }
 
-  fclose(fout);
+  fread(&image->infoheader, sizeof(InfoHeader), 1, fp);
+
+  // Check if it's a BMP file 24 bits uncompressed
+  if (image->infoheader.bits_per_pixel != 24 || image->infoheader.compression) {
+    return -1;
+  }
+
+  image->pixel = (Pixel*)malloc(sizeof(Pixel) * image->infoheader.cols * image->infoheader.rows);
+  totpixs = image->infoheader.rows * image->infoheader.cols;
+
+  for (int i = 0; i < totpixs; i += 512) {
+    fread(image->pixel + i, sizeof(Pixel), 512, fp);
+  }
+
+  fclose(fp);
+
+  return 0;
 }
 
-unsigned char blackandwhite(PIXEL p) {
-  return((unsigned char) (0.3*((float)p.red)+ 0.59*((float)p.green)+0.11*((float)p.blue)));
+int save_bmp(char *filename, Image *image) {
+  int totpixs;
+  FILE* fp = fopen(filename,"wb");
+
+  if (fp == NULL) {
+    return -1;
+  }
+
+  // Write header
+  fwrite(&image->header, sizeof(Header), 1, fp);
+
+  // Write header info
+  fwrite(&image->infoheader, sizeof(InfoHeader), 1, fp);
+
+  totpixs = image->infoheader.rows*image->infoheader.cols;
+  for (int i = 0; i < totpixs; i += 512) {
+    fwrite(image->pixel + i, sizeof(Pixel), 512, fp);
+  }
+
+  fclose(fp);
+
+  return 0;
 }
 
-void processBMP(IMAGE *imagefte, IMAGE *imagedst) {
-  int i,j;
-  int count=0;
-  PIXEL *pfte,*pdst;
-  PIXEL *v0,*v1,*v2,*v3,*v4,*v5,*v6,*v7; int imageRows,imageCols;
-  memcpy(imagedst,imagefte,sizeof(IMAGE)-sizeof(PIXEL *)); imageRows = imagefte->infoheader.rows;
-  imageCols = imagefte->infoheader.cols;
-  imagedst->pixel=(PIXEL *)malloc(sizeof(PIXEL)*imageRows*imageCols);
-  for(i=1;i<imageRows-1;i++)
-    for(j=1;j<imageCols-1;j++)
-    {
-      pfte=imagefte->pixel+imageCols*i+j;
-      v0=pfte-imageCols-1;
-      v1=pfte-imageCols;
-      v2=pfte-imageCols+1;
-      v3=pfte-1;
-      v4=pfte+1;
-      v5=pfte+imageCols-1;
-      v6=pfte+imageCols;
-      v7=pfte+imageCols+1;
+unsigned char black_and_white(Pixel p) {
+  float red = (float) p.red,
+        green = (float) p.green,
+        blue = (float) p.blue;
+  return (unsigned char) (0.3*red + 0.59*green + 0.11*blue);
+}
 
-      pdst=imagedst->pixel+imageCols*i+j;
-      if(abs(blackandwhite(*pfte)-blackandwhite(*v0))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v1))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v2))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v3))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v4))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v5))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v6))>DIF ||
-         abs(blackandwhite(*pfte)-blackandwhite(*v7))>DIF) {
-        pdst->red=0;
-        pdst->green=0;
-        pdst->blue=0;
-      }
-      else
-      {
-        pdst->red=255;
-        pdst->green=255;
-        pdst->blue=255;
+void process_bmp(Image *imagefte, Image *imagedst) {
+  int image_rows, image_cols;
+  Pixel *pfte, *pdst, *v0, *v1, *v2, *v3, *v4, *v5, *v6, *v7;
+
+  memcpy(imagedst, imagefte, sizeof(Image) - sizeof(Pixel*));
+  image_rows = imagefte->infoheader.rows;
+  image_cols = imagefte->infoheader.cols;
+  imagedst->pixel = (Pixel *)malloc(sizeof(Pixel) * image_rows * image_cols);
+
+  for (int i = 1; i < image_rows - 1; i++) {
+    for (int j = 1; j < image_cols - 1; j++) {
+      pfte = imagefte->pixel + image_cols*i + j;
+      v0 = pfte - image_cols - 1;
+      v1 = pfte - image_cols;
+      v2 = pfte - image_cols + 1;
+      v3 = pfte - 1;
+      v4 = pfte + 1;
+      v5 = pfte + image_cols - 1;
+      v6 = pfte + image_cols;
+      v7 = pfte + image_cols + 1;
+
+      pdst = imagedst->pixel + image_cols*i + j;
+      if (abs(black_and_white(*pfte) - black_and_white(*v0)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v1)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v2)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v3)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v4)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v5)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v6)) > DIF ||
+          abs(black_and_white(*pfte) - black_and_white(*v7)) > DIF) {
+        pdst->red = 0;
+        pdst->green = 0;
+        pdst->blue = 0;
+      } else {
+        pdst->red = 255;
+        pdst->green = 255;
+        pdst->blue = 255;
       }
     }
+  }
 }
 
 int main() {
@@ -152,34 +158,39 @@ int main() {
   long long start_ts;
   long long stop_ts;
   long long elapsed_time;
-  long lElapsedTime;
   struct timeval ts;
 
   gettimeofday(&ts, NULL);
-  start_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo inicial
+  start_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Initial time
 
-  strcpy(namedest,strtok(filename,"."));
-  strcat(filename,".bmp");
-  strcat(namedest,"_P.bmp");
-  printf("Archivo fuente %s\n",filename);
-  printf("Archivo destino %s\n",namedest);
+  strcpy(namedest, strtok(filename, "."));
+  strcat(filename, ".bmp");
+  strcat(namedest, "_P.bmp");
+  printf("Source file: %s\n", filename);
+  printf("Dest file: %s\n", namedest);
 
-  res=loadBMP(filename,&imagenfte);
+  res = load_bmp(filename, &source_image);
 
-  if(res==-1) {
-    fprintf(stderr,"Error al abrir imagen\n");
+  if (res == -1) {
+    fprintf(stderr,"Error when opening the image\n");
     exit(1);
   }
-  printf("Procesando imagen de: Renglones = %d, Columnas = %d\n",imagenfte.infoheader.rows,imagenfte.infoheader.cols);
 
-  processBMP(&imagenfte,&imagendst);
-  res=saveBMP(namedest,&imagendst);
-  if(res==-1) {
-    fprintf(stderr,"Error al escribir imagen\n");
+  printf("Processing image with %d rows and %d columns\n",
+      source_image.infoheader.rows, source_image.infoheader.cols);
+
+  process_bmp(&source_image, &dest_image);
+  res=save_bmp(namedest, &dest_image);
+
+  if (res == -1) {
+    fprintf(stderr,"Error when writting the image\n");
     exit(1);
   }
+
   gettimeofday(&ts, NULL);
-  stop_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo final
+  stop_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Final time
   elapsed_time = stop_ts - start_ts;
-  printf("Tiempo %2.3f segundos\n", (float)elapsed_time/1000000.0);
+  printf("Time: %2.3f seconds\n", (float)elapsed_time/1000000.0);
+
+  return 0;
 }
