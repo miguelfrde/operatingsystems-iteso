325c51f41dceea26a1140e4edeba437d1ae03fb3
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-10-10 10:32:50 -0500

    Spacing fixes

--- src/practica4/virtual_processor.h
+++ src/practica4/virtual_processor.h
@@ -1,12 +1,12 @@
-#define MAXPROC 3 
+#define MAXPROC 3
 
 #define NINGUNO -1
 
 #define NUEVO 0
-#define LISTO 1 
+#define LISTO 1
 #define EJECUCION 2
 #define BLOQUEADO 3
-#define TERMINADO 4 
+#define TERMINADO 4
 
 #define TIMER 0
 #define SOLICITA_E_S 1
@@ -16,21 +16,21 @@
 #define PROCESO_TERMINADO 4
 
 struct PROCESO {
-    int pid;
-    int tinicio;
-    int tcpu;
-    int tes;
-    int trestante;
-    int prioridad;
-    int estado;
+  int pid;
+  int tinicio;
+  int tcpu;
+  int tes;
+  int trestante;
+  int prioridad;
+  int estado;
 };
 
 struct COLAPROC {
-    int cola[20];
-    int ent;
-    int sal;
+  int cola[20];
+  int ent;
+  int sal;
 };
 
-void mete_a_cola(struct COLAPROC *q,int proceso);
+void mete_a_cola(struct COLAPROC *q, int proceso);
 int cola_vacia(struct COLAPROC q);
 int sacar_de_cola(struct COLAPROC *q);
