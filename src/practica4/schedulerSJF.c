3f1ec947d0a59e3cd7a5a4ab4f080a31467e03e0
Alberto Arias Azcona <maug.thief@gmail.com>
2015-10-14 21:07:53 -0500

    Cleans code

--- src/practica4/schedulerSJF.c
+++ src/practica4/schedulerSJF.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
 #include <limits.h>
 #include "virtual_processor.h"
@@ -13,63 +12,55 @@ int queue_size(struct COLAPROC q);
 
 int scheduler(int evento) {
 
-  bool cambia_proceso = false;
   int prox_proceso_a_ejecutar = pars[1];
 
   if (evento == PROCESO_NUEVO) {
     proceso[pars[0]].estado = LISTO;
     mete_a_cola(&listos, pars[0]);
-    cambia_proceso = true;
   }
 
   if (evento == TIMER) {
     printf("Llega interrupcion del Timer\n");
     proceso[pars[0]].estado = LISTO;
-    cambia_proceso = true;
   }
 
   if (evento == SOLICITA_E_S) {
     proceso[pars[1]].estado = BLOQUEADO;
     printf("Solicita E/S Proceso %d\n", pars[1]);
-    cambia_proceso = true;
   }
 
   if (evento == TERMINA_E_S) {
     proceso[pars[0]].estado = LISTO;
-    cambia_proceso = true;
     printf("Termina E/S Proceso desbloqueado %d\n", pars[0]);
   }
 
   if (evento == PROCESO_TERMINADO) {
     proceso[pars[0]].estado = TERMINADO;
-    cambia_proceso = true;
   }
 
-  if (cambia_proceso) {
-    if (!cola_vacia(listos)){
-    	int tmp_proc;
-    	int min = INT_MAX;
-    	prox_proceso_a_ejecutar = NINGUNO;
-    	for(int i=0; i<queue_size(listos); i++){
-    		tmp_proc = sacar_de_cola(&listos);
-    		if(proceso[tmp_proc].estado == TERMINADO){
-					continue;
-    		}
-
-    		if(min >= proceso[tmp_proc].trestante){
-    			min = proceso[tmp_proc].trestante;
-    			prox_proceso_a_ejecutar = tmp_proc;
-    		}
-    		mete_a_cola(&listos, tmp_proc);
-
+  if (!cola_vacia(listos)){
+  	int tmp_proc;
+ 		int min = INT_MAX;
+  	prox_proceso_a_ejecutar = NINGUNO;
+    for(int i=0; i<queue_size(listos); i++){
+    	tmp_proc = sacar_de_cola(&listos);
+    	if(proceso[tmp_proc].estado == TERMINADO){
+				continue;
     	}
-      proceso[prox_proceso_a_ejecutar].estado = EJECUCION;
-      cambia_proceso = false;
+
+    	if(min >= proceso[tmp_proc].trestante){
+    		min = proceso[tmp_proc].trestante;
+    		prox_proceso_a_ejecutar = tmp_proc;
+    	}
+    	mete_a_cola(&listos, tmp_proc);
+
     }
-    if (prox_proceso_a_ejecutar == NINGUNO){
-    	printf("Ho hay procesos en cola\n");
-    }
+    proceso[prox_proceso_a_ejecutar].estado = EJECUCION;
   }
+  if (prox_proceso_a_ejecutar == NINGUNO){
+    printf("Ho hay procesos en cola\n");
+  }
+
   return prox_proceso_a_ejecutar;
 }
 
