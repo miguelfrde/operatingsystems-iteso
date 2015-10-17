db61c10d2135e268a68f81d41f5388120815d6c5
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-10-17 12:06:15 -0500

    Replace tabs by two spaces

--- src/practica5/hw_solution_processes.c
+++ src/practica5/hw_solution_processes.c
@@ -6,10 +6,10 @@
 #include <sys/wait.h>
 
 #define atomic_xchg(A,B) __asm__ __volatile__( \
-		" lock xchg %1,%0 ;\n" \
-		: "=ir" (A) \
-		: "m" (B), "ir" (A) \
-		);
+    " lock xchg %1,%0 ;\n" \
+    : "=ir" (A) \
+    : "m" (B), "ir" (A) \
+    );
 
 #define ITERS 10
 
@@ -17,61 +17,61 @@ char *country[3] = {"Peru","Bolivia","Colombia"};
 int *g;
 
 void process(int i) {
-	int l;
+  int l;
 
-	for (int k = 0; k < ITERS; k++) {
-		l = 1;
+  for (int k = 0; k < ITERS; k++) {
+    l = 1;
 
     do {
-			atomic_xchg(l,*g);
-		} while (l != 0);
+      atomic_xchg(l,*g);
+    } while (l != 0);
 
     // Begin critical section
-		printf("Entra %s", country[i]);
-		fflush(stdout);
-		sleep(rand() % 3);
-		printf(" - Sale %s\n", country[i]);
+    printf("Entra %s", country[i]);
+    fflush(stdout);
+    sleep(rand() % 3);
+    printf(" - Sale %s\n", country[i]);
     // Finish critical section
 
-		l = 1;
-		*g = 0;
+    l = 1;
+    *g = 0;
 
     // Random wait outside of the critical section
-		sleep(rand() % 3);
-	}
+    sleep(rand() % 3);
+  }
 }
 
 int main(int argc, char* argv[]) {
-	int pid;
-	int status;
-	int shmid;
+  int pid;
+  int status;
+  int shmid;
 
   // Request the shared memory
-	shmid = shmget(0x1234, sizeof(g), 0666 | IPC_CREAT);
-	if (shmid == -1) {
-		perror("Error en la memoria compartida\n");
-		exit(1);
-	}
+  shmid = shmget(0x1234, sizeof(g), 0666 | IPC_CREAT);
+  if (shmid == -1) {
+    perror("Error en la memoria compartida\n");
+    exit(1);
+  }
 
-	// Connect the variable to the shared memory
-	g = shmat(shmid, NULL, 0);
-	if (g == NULL) {
-		perror("Error en el shmat\n");
-		exit(2);
-	}
+  // Connect the variable to the shared memory
+  g = shmat(shmid, NULL, 0);
+  if (g == NULL) {
+    perror("Error en el shmat\n");
+    exit(2);
+  }
 
-	*g = 0;
-	srand(getpid());
+  *g = 0;
+  srand(getpid());
 
-  for	(int i = 0; i < 3; i++) {
-		pid = fork();
-		if (pid==0) {
-			process(i);
+  for  (int i = 0; i < 3; i++) {
+    pid = fork();
+    if (pid==0) {
+      process(i);
     }
-	}
+  }
 
-	for (int i = 0; i < 3; i++) {
-		pid = wait(&status);
+  for (int i = 0; i < 3; i++) {
+    pid = wait(&status);
   }
 
   // Delete the shared memory
--- src/practica5/hw_solution_threads.c
+++ src/practica5/hw_solution_threads.c
@@ -6,9 +6,9 @@
 // Macro for the machine instruction xchg
 #define atomic_xchg(A,B) __asm__ __volatile__( \
     " lock xchg %1,%0 ;\n" \
-		: "=ir" (A) \
-		: "m" (B), "ir" (A) \
-		);
+    : "=ir" (A) \
+    : "m" (B), "ir" (A) \
+    );
 #define ITERS 10
 
 
@@ -18,29 +18,29 @@ int g = 0;
 
 void *thread1(void *arg) {
   int *mynum = (int *)arg;
-	int i = *mynum;
-	int l;
+  int i = *mynum;
+  int l;
 
-	for (int k = 0; k < ITERS; k++) {
-		l = 1;
+  for (int k = 0; k < ITERS; k++) {
+    l = 1;
 
-		do {
-			atomic_xchg(l, g);
-		} while (l != 0);
+    do {
+      atomic_xchg(l, g);
+    } while (l != 0);
 
-		// Begin critical section
+    // Begin critical section
     printf("Entra %s", country[i]);
-		fflush(stdout);
-		sleep(rand() % 3);
-		printf(" - Sale %s\n", country[i]);
-		// Finish critical section
+    fflush(stdout);
+    sleep(rand() % 3);
+    printf(" - Sale %s\n", country[i]);
+    // Finish critical section
 
     g = 0;
-		l = 1;
+    l = 1;
 
     // Random wait outside of the critical section
-		sleep(rand() % 3);
-	}
+    sleep(rand() % 3);
+  }
 
   pthread_exit(NULL);
 }
@@ -54,13 +54,13 @@ int main(int argc, char* argv[]) {
 
   // Create the threads
   for (int i = 0; i < 3; i++) {
-  	args[i] = i;
- 	  pthread_create(&tid[i], NULL, thread1, (void *) &args[i]);
+    args[i] = i;
+     pthread_create(&tid[i], NULL, thread1, (void *) &args[i]);
   }
 
-	// Wait for the threads to finish
+  // Wait for the threads to finish
   for (int i = 0; i < 3; i++) {
-  	pthread_join(tid[i], &thread_result);
+    pthread_join(tid[i], &thread_result);
   }
 
   return 0;
