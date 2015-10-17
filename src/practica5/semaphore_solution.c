1af4aec6bc2255817bcf3ec888de7848fc8b99a8
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-10-17 14:17:08 -0500

    Change tabs for two spaces

--- src/practica5/semaphore_solution.c
+++ src/practica5/semaphore_solution.c
@@ -9,18 +9,18 @@ char *country[3] = {"Peru","Bolivia","Colombia"};
 Semaphore *sem;
 
 void proceso(int i) {
-	for (int k = 0; k < ITERS; k++) {
-		waitsem(sem);
-		printf("Entra %s ", country[i]);
-		fflush(stdout);
-		sleep(rand() % 3);
-		printf("- Sale %s\n", country[i]);
-		signalsem(sem);
-		sleep(rand() % 3);
-	}
-	exit(0);
+  for (int k = 0; k < ITERS; k++) {
+    waitsem(sem);
+    printf("Entra %s ", country[i]);
+    fflush(stdout);
+    sleep(rand() % 3);
+    printf("- Sale %s\n", country[i]);
+    signalsem(sem);
+    sleep(rand() % 3);
+  }
+  exit(0);
 }
 
 int main(int argc, char* argv[]) {
-	initsem(sem, 1);
+  initsem(sem, 1);
 }
