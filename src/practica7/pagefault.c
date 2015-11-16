25bf8a87001efc9398f8c02a40bd7bae684bcced
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-11-15 20:01:05 -0600

    Improve pagefault.c style

--- src/practica7/pagefault.c
+++ src/practica7/pagefault.c
@@ -14,48 +14,46 @@ extern struct PROCESSPAGETABLE processpagetable[];
 
 // Rutina de fallos de página
 
-int pagefault(char *vaddress)
-{
-    int i;
-    int frame;
-    int pag_del_proceso;
+int pagefault(char *vaddress) {
+  int i;
+  int frame;
+  int pag_del_proceso;
 
-    // Calcula la página del proceso
-    pag_del_proceso=(int) vaddress>>12;
-    // Cuenta los marcos asignados al proceso
-    i=countframesassigned();
+  // Calcula la página del proceso
+  pag_del_proceso = (int) vaddress >> 12;
+  // Cuenta los marcos asignados al proceso
+  i = countframesassigned();
 
-    // Busca un marco libre en el sistema
-    frame=getfreeframe();
+  // Busca un marco libre en el sistema
+  frame = getfreeframe();
 
-    if(frame==-1)
-    {
-        return(-1); // Regresar indicando error de memoria insuficiente
+  if (frame == -1) {
+    return -1; // Regresar indicando error de memoria insuficiente
+  }
+
+  processpagetable[pag_del_proceso].presente = 1;
+  processpagetable[pag_del_proceso].framenumber = frame;
+
+
+  return 1; // Regresar todo bien
+}
+
+
+int getfreeframe() {
+  int i;
+  // Busca un marco libre en el sistema
+  for (i = 0; i < systemframetablesize; i++) {
+    if (!systemframetable[i].assigned) {
+      systemframetable[i].assigned = 1;
+      break;
     }
+  }
 
+  if (i < systemframetablesize) {
+    systemframetable[i].assigned = 1;
+  } else {
+    i = -1;
+  }
 
-    processpagetable[pag_del_proceso].presente=1;
-    processpagetable[pag_del_proceso].framenumber=frame;
-
-
-    return(1); // Regresar todo bien
+  return i;
 }
-
-
-int getfreeframe()
-{
-    int i;
-    // Busca un marco libre en el sistema
-    for(i=0;i<systemframetablesize;i++)
-        if(!systemframetable[i].assigned)
-        {
-            systemframetable[i].assigned=1;
-            break;
-        }
-    if(i<systemframetablesize)
-        systemframetable[i].assigned=1;
-    else
-        i=-1;
-    return(i);
-}
-
