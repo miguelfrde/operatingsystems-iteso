07e4b851a8b01fc5304dab570b07f58b63b0663c
Setzer777 <lguitarras0594@gmail.com>
2015-11-30 20:04:04 -0600

    Not writing to the virtual disk, but in theory it should

--- src/practica8/vdfdisk.c
+++ src/practica8/vdfdisk.c
@@ -11,11 +11,31 @@ This program will create the data structures and necessary tables in the first d
 
 #define SECSIZE 512
 int writeToSector(MBR sector, int driveNumber){
+  
   char buffer[SECSIZE];
-  /*
-  strcpy(buffer, sector.bootstrap_code);
-  strcat(buffer, sector.partition[0].drive_status);
-  */
+  
+  printf("%s", sector.bootstrap_code);
+  /*for(unsigned int i = 0; i<sizeof(sector); i++){
+    buffer[i] = *((char*)(&sector) + i); 
+  }*/
+  memcpy(buffer, sector.bootstrap_code, sizeof(sector));
+
+  //debugging what's inside the buffer
+  for(unsigned int i = 0; i<sizeof(sector); i++){
+    printf("%c", buffer[i]); 
+  }
+
+  if(vdwritesector(driveNumber, 0, 0, 1, 1, buffer) == -1){
+    printf("Error al escribir\n"); 
+  }
+ 
+  char readBuffer[SECSIZE];
+  if(vdreadsector(driveNumber, 0, 0, 1, 1, readBuffer) == -1){
+    printf("Error al leer\n"); 
+  }
+  
+  printf("Read buffer: \n%s\n", readBuffer); 
+
   return 0;
 }
 
@@ -26,33 +46,40 @@ int main(int argc, char *argv[]){
   //MBR
   MBR firstSector;
   char bootstrap_code[446];
-  strcpy(bootstrap_code, "ASSEMBLY CODE HERE");
+  for (int i = 0; i < 447; i++){
+    bootstrap_code[i] = 'A';
+  }
+  //strcpy(bootstrap_code, "ASSEMBLY CODE HERE");
   PARTITION partition;
-  short boot_signature;
+  short boot_signature = 1;
 
   //Partition default values
-  int drive_status = 1;
+  int drive_status = 49;
   char chs_begin[3];
-  strcpy(chs_begin, "000");
-  char partition_type = 'f';
+  strcpy(chs_begin, "00P");
+  char partition_type = 'A';
   char chs_end[3];
-  strcpy(chs_end, "FFF");
-  int lba = 0;
-  int secs_partition = 0;
-  
-
+  strcpy(chs_end, "NDA");
+  int lba = 48;
+  int secs_partition = 48;
+ 
   //Initialize partition
   partition.drive_status = drive_status;
   strcpy(partition.chs_begin, chs_begin); 
-  partition.partition_type = 'F';
+  partition.partition_type = partition_type;
   strcpy(partition.chs_end, chs_end);
   partition.lba = lba;
   secs_partition = secs_partition;
 
+
   //Initialize MBR
   strcpy(firstSector.bootstrap_code, bootstrap_code);
   firstSector.partition[0] = partition;
+  firstSector.partition[1] = partition;
+  firstSector.partition[2] = partition;
+  firstSector.partition[3] = partition;
+  firstSector.boot_signature = boot_signature;
 
-  //writeToSector(firstSector, driveNumber);
+  writeToSector(firstSector, driveNumber);
   return 0;
 }
