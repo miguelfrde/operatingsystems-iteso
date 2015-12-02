9c87e1415dd726a360db233dde32ab06daed2999
Setzer777 <lguitarras0594@gmail.com>
2015-12-02 00:43:53 -0600

    vdfdisk.c now writes the MasterBootRecord Structure into the first sector

--- src/practica8/vdfdisk.c
+++ src/practica8/vdfdisk.c
@@ -11,35 +11,17 @@ This program will create the data structures and necessary tables in the first d
 
 #define SECSIZE 512
 
-void printBuffer(unsigned char* buffer) {
-  for (int i = 0; i < SECSIZE; i++) {
-    printf("%c", buffer[i]);
-  }
-}
-
 int writeToSector(MBR *sector, int driveNumber) {
   unsigned char buffer[SECSIZE];
-  unsigned char readBuffer[SECSIZE];
 
-  printf("%s", sector->bootstrap_code);
-  for (int i = 0; i < sizeof(sector); i++) {
-    buffer[i] = *((char*)(sector) + i);
+  for (int i = 0; i < sizeof(MBR); i++) {
+    buffer[i] = *((char*)sector + i);
+    printf("%c", buffer[i]);
   }
-
-  //memcpy(buffer, &sector, SECSIZE);
-  printBuffer(buffer);
-  //debugging what's inside the buffer
 
   if (vdwritesector(driveNumber, 0, 0, 1, 1, buffer) == -1) {
     printf("Error al escribir\n");
   }
-
-  if (vdreadsector(driveNumber, 0, 0, 1, 1, readBuffer) == -1) {
-    printf("Error al leer\n");
-  }
-
-  printf("\n\n\n\n");
-  printBuffer(readBuffer);
 
   return 0;
 }
@@ -51,11 +33,11 @@ int main(int argc, char *argv[]) {
   //MBR
   MBR firstSector;
   char bootstrap_code[446];
-  for (int i = 0; i < 447; i++) {
+  
+  for (int i = 0; i < 446; i++) {
     bootstrap_code[i] = 'A';
   }
 
-  //strcpy(bootstrap_code, "ASSEMBLY CODE HERE");
   PARTITION partition;
   short boot_signature = 1;
 
@@ -66,8 +48,8 @@ int main(int argc, char *argv[]) {
   char partition_type = 'A';
   char chs_end[3];
   strcpy(chs_end, "NDA");
-  int lba = 48;
-  int secs_partition = 48;
+  int lba = 49; //ascci value: 1
+  int secs_partition = 49; 
 
   //Initialize partition
   partition.drive_status = drive_status;
@@ -76,7 +58,6 @@ int main(int argc, char *argv[]) {
   strcpy(partition.chs_end, chs_end);
   partition.lba = lba;
   secs_partition = secs_partition;
-
 
   //Initialize MBR
   strcpy(firstSector.bootstrap_code, bootstrap_code);
